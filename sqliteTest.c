a37ffc48c45d17e668e32badb480c32187c28d4f
AoyaSora <s24g1125jk@s.chibakoudai.jp>
2026-05-06 17:26:55 +0900

    diary用のinsert関数を作成した．次はtoDo用のinsert関数

--- sqliteTest.c
+++ sqliteTest.c
@@ -56,46 +56,48 @@ int createTable(sqlite3 *db, const char* tableName, const char* columns){
     return SQLITE_OK;
 }
 
-int insertTable(sqlite3* db,  char* tableName, const char* columns){
+int insertDiaryTable(sqlite3* db, const char* values[]){
     sqlite3_stmt* stmt;
-    char sql[512];
-    char placeholders[256];
+    const char* sql = "INSERT INTO diary(date,title,content,created_at,updated_at) VALUES (?,?,?,?,?);";
     int rc;
 
-    int colCount = count_columns(columns);
-    make_placeholders(placeholders,colCount);
-    //sqlに準備文の埋め込み
-    snprintf(sql,sizeof(sql),"INSERT INTO %s(%s) VALUES (%s);",tableName,columns,placeholders);
     // INSERT文の準備
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
     if(rc!= SQLITE_OK) return rc;
-
     
-    // sqlite3_prepare_v2(sqlite3 *db, const void* zSal, int nByte, sqlite3_stmt** ppStmt, const char** pzTail)
-    /*
+    // bindで'?'に挿入
+    for(int i=0; i< 5; i++){
+        sqlite3_bind_text(stmt,i+1,values[i],-1,SQLITE_STATIC);
+    }
+    
+    // step実行
+    rc = sqlite3_step(stmt);// ここでのエラーは関数を使用するときに書く．
+    
+    // 後処理
+    sqlite3_finalize(stmt);
+    return rc;
+
+    /*  sqlite3_prepare_v2(sqlite3 *db, const void* zSal, int nByte, sqlite3_stmt** ppStmt, const char** pzTail)
     第一引数 db: データベース接続
     第二引数 zSql: コンパイルするステートメント
     第三引数 nByte: -1の場合，最初のゼロ終端文字まで読み込む．nByteが正の場合，zSqlから読み込まれる最大バイト数，nByteが0の場合，準備済みステートメントは生成されない．呼び出し元が指定された文字列がnull終端されている事を知っている場合，null終端文字を含む入力文字列のバイト数であるnByteパラメータを渡すとパフォーマンスがわずかに向上する．
     第四引数 *ppStmt: sqlite3_step()を利用して実行できるコンパイル済みの準備積みステートメントを指すように設定されている．エラー発生時，*ppStmtはnULLになる．ppStmtはNULLにできない．
     第五引数 pzTail: NULLでない場合，*pzTailはzSql内の最初のSQL文の末尾の次の最初のバイトを指す．複数のsql文をloopで実行する場合に使用する
-    
     */
-    // sqlite3_prepare_v2() -> sqlite3_bind_text() -> sqlite3_step(stmt) -> sqlite3_finalize(stmt)
-}
-//columnの数を数える '\0'で終了
-int count_columns(const char *columns) {
-    int count = 1;
-    for (int i = 0; columns[i]; i++) {
-        if (columns[i] == ',') count++;
-    }
-    return count;
-}
-// columnの数だけ準備文のための?,の文字列を作成する 
-void make_placeholders(char *buf, int n) {
-    buf[0] = '\0';
+   /*   sqlite3_bind_*(sqlite3_stmt*, )
+   第一引数 sqlite3_stmt: 常にsqlite3_prepare_v2()またはその派生関数から返されるsqlite3_stmtオブジェクトへのポインタ
+   第二引数 : 設定するSQlパラメータのインデックス．(?,?,)の場所一番左が1同じ名前のSQLパラメータが複数回使用される場合，二度目以降の出現場所h最初の出現箇所と同じインデックスになる．
+   第三引数 : パラメータにバインドする値．
+   第四引数 : パラメータのバイト数．負の場合は最初のゼロ終端までのバイト数．未定義もある．
+   使用するとしたらsqlite3_bind_int,sqlite3_bind_textぐらい?
+   第五引数 : 3番目のパラメータによって参照されるオブジェクトの有効期間を制御または示す．
+   */
+    //以下のような内容をforで回している
+    // sqlite3_bind_text(stmt,1,"2026/5/5",-1,SQLITE_STATIC);
+    // sqlite3_bind_text(stmt,2,"バク転の進捗とtoDoの結果",-1,SQLITE_STATIC);
+    // sqlite3_bind_text(stmt,3,"いいかんじ，後ろにジャンプして空中でブリッジをする感覚でやるといいかも．",-1,SQLITE_STATIC);
+    // sqlite3_bind_text(stmt,4,"20:23",-1,SQLITE_STATIC);
+    // sqlite3_bind_text(stmt,5,"20:40",-1,SQLITE_STATIC);
 
-    for (int i = 0; i < n; i++) {
-        strcat(buf, "?");   // '\0'の前に追加
-        if (i != n - 1) strcat(buf, ", "); // 最後の?じゃなかったら ','を追加
-    }
+    // sqlite3_prepare_v2() -> sqlite3_bind_text() -> sqlite3_step(stmt) -> sqlite3_finalize(stmt)
 }
